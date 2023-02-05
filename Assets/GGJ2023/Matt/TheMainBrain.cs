7a802ddc18600beb41cba8ebbc19d8f4d675042f
MattT <main@MattTs-MacBook-Pro.local>
2023-02-04 21:52:50 -0500

    progress

---
+++ Assets/GGJ2023/Matt/TheMainBrain.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class PlayerObject
+{
+    public GameObject prefab;
+    public Color color;
+}
+public class TheMainBrain : MonoBehaviour
+{
+    public PlayerObject[] players;
+}
+
